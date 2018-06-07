62aedb7a7c2c924f959fc23556eba1f02b0717a8
yshwang <acafela91@gmail.com>
2018-06-07 23:21:12 +0900

    main method 추가

--- push-simple-service/src/main/java/push/simple/service/APNsConnector.java
+++ push-simple-service/src/main/java/push/simple/service/APNsConnector.java
@@ -2,6 +2,8 @@ package push.simple.service;
 
 import java.util.List;
 
+import org.json.JSONException;
+
 import javapns.Push;
 import javapns.communication.exceptions.CommunicationException;
 import javapns.communication.exceptions.KeystoreException;
@@ -18,6 +20,11 @@ public class APNsConnector {
 		this.keystorePassword = keystorePassword;
 	}
 	
+	public PushedNotifications send(String message, List<String> pushKeys) throws CommunicationException, KeystoreException, JSONException{
+		PushNotificationPayload payload = buildDefaultPayload(message);
+		return send(payload, pushKeys, true);
+	}
+	
 	public PushedNotifications send(PushNotificationPayload payload, List<String> pushKeys) throws CommunicationException, KeystoreException{
 		return send(payload, pushKeys, true);
 	}
@@ -25,4 +32,13 @@ public class APNsConnector {
 	public PushedNotifications send(PushNotificationPayload payload, List<String> pushKeys, boolean productionServer) throws CommunicationException, KeystoreException{
 		return Push.payload(payload, keystorePath, keystorePassword, productionServer, pushKeys);
 	}
+	
+	private PushNotificationPayload buildDefaultPayload(String message) throws JSONException {
+		PushNotificationPayload payload = new PushNotificationPayload();
+		payload.addAlert(message);
+		payload.addBadge(1);
+		payload.addSound("default");
+		return payload;
+	}
+
 }
